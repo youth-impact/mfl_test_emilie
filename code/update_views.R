ae5384433dacfbfa857455a2ce9d742fa9a37291
Jake Hughey <jakejhughey@gmail.com>
2023-07-30 08:36:55 +0530

    updated code to enable testing and production environments within one repo.

--- code/update_views.R
+++ code/update_views.R
@@ -1,7 +1,12 @@
 source(file.path('code', 'setup.R'))
 
-msg = tryCatch(
-  update_views(params), error = \(e) {
+# TODO: facilitator_database_main gsheet triggers gh actions in prod
+
+msg = tryCatch({
+  params = get_params()
+  update_views(params)
+  },
+  error = \(e) {
     message(as.character(e))
     paste("Error encountered. Please check the GitHub Actions",
           "workflow log and the Google Sheet's version history.")
